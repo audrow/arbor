54beed61593cee7bc852fd472906a7888df71792
Audrow Nash <audrow@hey.com>
2022-03-27 12:51:01 -0400

    Walk package xmls and modify them
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+import render from 'dom-serializer'
+import {cloneNode, Text} from 'domhandler'
 import fs from 'fs'
 import gitClone from 'git-clone/promise'
+import glob from 'glob'
+import {DomUtils, parseDocument} from 'htmlparser2'
 import yaml from 'js-yaml'
 import fetch from 'node-fetch'
 import {join} from 'path'
+import format from 'xml-formatter'
 import log from './logger'
 import validateSchema from './utils/validate-schema'
 import {ReposFileSchema} from './__schemas__/ReposFile'
@@ -10,9 +15,9 @@ import type RepoInfo from './__types__/RepoInfo'
 import type ReposFile from './__types__/ReposFile'
 
 const cacheDirectory = join(process.cwd(), '.cache')
-const reposFilePath = join(cacheDirectory, 'repos.yaml')
-const reposUrl = 'https://raw.githubusercontent.com/ros2/ros2/master/ros2.repos'
+const cachedReposFilePath = join(cacheDirectory, 'repos.yaml')
 const cachedReposDirectory = join(cacheDirectory, 'repos')
+const reposUrl = 'https://raw.githubusercontent.com/ros2/ros2/master/ros2.repos'
 
 async function getReposFile(reposUrl: string, reposFilePath: string) {
   const data = await fetch(reposUrl)
@@ -37,10 +42,10 @@ async function setupCache() {
     async () => log.debug(`Cache directory already exists`),
   )
   await doIfPathDoesNotExist(
-    reposFilePath,
+    cachedReposFilePath,
     async () => {
-      await getReposFile(reposUrl, reposFilePath),
-        log.debug(`Downloaded repos file: ${reposFilePath}`)
+      await getReposFile(reposUrl, cachedReposFilePath),
+        log.debug(`Downloaded repos file: ${cachedReposFilePath}`)
     },
     async () => log.debug(`Repos file already exists`),
   )
@@ -79,10 +84,46 @@ async function doIfPathDoesNotExist(
 async function main() {
   await setupCache()
 
-  const reposFile = loadReposFile(reposFilePath)
+  const reposFile = loadReposFile(cachedReposFilePath)
   Object.entries(reposFile.repositories)
     .slice(0, 2)
     .map(async ([key, info]) => await cloneRepo(key, info))
+
+  // TODO find paths to package xmls
+  const packageXmlPaths = glob.sync('**/package.xml', {
+    cwd: cachedReposDirectory,
+    realpath: true,
+  })
+  const packageXml = fs.readFileSync(packageXmlPaths[0], 'utf8')
+  console.log(packageXmlPaths[0])
+  console.log(packageXml)
+
+  const dom = parseDocument(packageXml)
+  console.log(dom)
+
+  const maintainers = DomUtils.getElementsByTagName('maintainer', dom)
+  const newMaintainerData = {
+    name: 'Audrow Nash',
+    email: 'audrow@hey.com',
+  }
+  const newMaintainer = cloneNode(maintainers[0], true)
+  newMaintainer.attribs.email = newMaintainerData.email
+  ;(newMaintainer.children[0] as Text).data = newMaintainerData.name
+
+  const newAuthor = cloneNode(maintainers[0], true)
+  newAuthor.tagName = 'author'
+  const authors = DomUtils.getElementsByTagName('author', dom)
+  DomUtils.append(authors[0], newAuthor)
+
+  DomUtils.append(maintainers[1], newMaintainer)
+  DomUtils.removeElement(maintainers[1])
+  let out = render(dom, {selfClosingTags: true})
+  out = format(out, {
+    indentation: '  ',
+    collapseContent: true,
+    whiteSpaceAtEndOfSelfclosingTag: true,
+  })
+  console.log(out)
 }
 
 main()
